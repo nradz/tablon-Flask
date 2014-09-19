1d4ddff2135e0abae12ad89d5171b64bd000aeec
Anrádez <nradz@mint>
2014-09-19 12:11:53 +0200

    Entradas

--- tablon.py
+++ tablon.py
@@ -4,6 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+entradas = [ 'Aprendiendo Flask',
+'He encontrado un pen drive azul en la sala de estudios y lo he llevado a objetos perdidos',
+'HOYGAN, ALGUIEN SAVE KOMO JAQUEAR FLASK¿',
+'KDD en la Alfalfa. Que venga quien quiera.',
+'Apuntes de ADA en consigna. El archivo es ADA.zip y la contraseña es "rosa"'
+]
 
 @app.route('/')
 def index():
